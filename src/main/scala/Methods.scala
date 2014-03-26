29739590e124ebd527cc093fb6cb27aa5454d47f
softprops <d.tangren@gmail.com>
2014-03-26 00:43:20 -0400

    more v2

--- src/main/scala/Methods.scala
+++ src/main/scala/Methods.scala
@@ -9,14 +9,33 @@ import org.json4s.JsonDSL._
 
 trait Methods { self: Requests =>
 
+  // view_group scope
+  object emoticons {
+    private[this] def emoticonBase = apiBase / "emoticon"
+
+    def apply[T]
+      (start: Int = 0, max: Int = 100, kind: String = "all")
+      (handler: Client.Handler[T])
+      (implicit ec: ExecutionContext) =
+      request(emoticonBase <<? Map(
+        "start-index" -> start.toString,
+        "max-results" -> max.toString,
+        "type" -> kind))(handler)
+
+    def get(emo: String) =
+      complete(emoticonBase / emo)
+  }
+
   object rooms {
     private[this] def roomBase = apiBase / "room"
-    private[this] def roomsBase = apiBase / "rooms"
-    
-    def delete(id: String) =
-      complete(roomsBase.POST / "delete" << Map("room_id" -> id))
 
     // manage_rooms scope
+    /** https://www.hipchat.com/docs/apiv2/method/delete_room */
+    def delete(id: String) =
+      complete(roomBase.DELETE /  id)
+
+    // manage_rooms scope
+    /** https://www.hipchat.com/docs/apiv2/method/create_room */
     def create(
       name: String,
       ownerId: Option[String] = None,
@@ -29,12 +48,16 @@ trait Methods { self: Requests =>
                ("privacy" -> Privacy(public)) ~
                ("guest_access" -> guests))))
 
+    /** https://www.hipchat.com/docs/apiv2/method/update_room */
+    //def update()
+
     // view_group scope
     def list(start: Int = 0, max: Int = 100) =
       complete(roomBase <<? Map("start-index" -> start.toString,
                                 "max-results" -> max.toString))
 
     // send_notification scope
+    /** https://www.hipchat.com/docs/apiv2/method/send_room_notification */
     def notify(
       room: String,
       message: String,
@@ -48,23 +71,107 @@ trait Methods { self: Requests =>
                ("notify" -> notify) ~
                ("message_format" -> format))))
 
+    /** https://www.hipchat.com/docs/apiv2/method/get_room */
+    def get(room: String) =
+      complete(roomBase / room)
 
-    def topic(
-      room: String,
-      topic: String,
-      from: String = "API") =
-      complete(roomsBase.POST / "topic"
-             << Map(
-               "room_id" -> room,
-               "topic" -> topic,
-               "from" -> from))
+    // admin_room scope
+    case class Webhooks(room: String) {
+      private[this] def webhookBase = apiBase / "room" / room / "webhook"
 
-    def show(room: String) =
-      complete(roomsBase / "show" <<? Map("room_id" -> room))
+      /** https://www.hipchat.com/docs/apiv2/method/get_all_webhooks */
+      def apply[T]
+        (start: Int = 0, max: Int = 100)
+        (handler: Client.Handler[T])
+        (implicit ec: ExecutionContext) =
+        request(webhookBase <<? Map(
+          "start-index" -> start.toString,
+          "max-results" -> max.toString))(handler)
+
+      /** https://www.hipchat.com/docs/apiv2/method/get_webhook */
+      def get(hook: String) =
+        complete(webhookBase / hook)
+
+      /** https://www.hipchat.com/docs/apiv2/method/create_webhook */
+      def create(
+        url: String,
+        pattern: String,
+        event: String,
+        name: String) =
+        complete(webhookBase.POST << compact(render(
+          ("url" -> url) ~
+          ("pattern" -> pattern) ~
+          ("event" -> event) ~
+          ("name" -> name))))
+
+      /** https://www.hipchat.com/docs/apiv2/method/delete_webhook */
+      def delete(hook: String) =
+        complete(webhookBase.DELETE / hook)
+    }
+
+    def webhooks(room: String) = Webhooks(room)
+
+    // admin_room scope
+    case class Members(room: String) {
+      private[this] def memberBase = apiBase / "room" / room / "member"
+
+      /** https://www.hipchat.com/docs/apiv2/method/get_all_members */
+      def apply[T]
+        (start: Int = 0, max: Int = 100)
+        (handler: Client.Handler[T])
+        (implicit ec: ExecutionContext) =
+        complete(memberBase <<? Map(
+          "start-index" -> start.toString,
+          "max-results" -> max.toString))
+
+      /** https://www.hipchat.com/docs/apiv2/method/add_member */
+      def add(user: String) =
+        complete(memberBase.PUT / user)
+
+      /** https://www.hipchat.com/docs/apiv2/method/remove_member */
+      def remove(user: String) =
+        complete(memberBase.DELETE / user)
+
+      def invite(user: String) =
+        complete(apiBase.POST / "room" / room / "invite" / user)
+    }
+
+    def members(room: String) = Members(room)
+
+    def topic(room: String, name: String) =
+      complete(apiBase.PUT / room / "topic" << compact(render(
+        "topic" -> name)))
   }
 
   object users {
-    private[this] def usersBase = apiBase / "users"
+    private[this] def userBase = apiBase / "user"
+
+    case class User(user: String) {
+      private[this] def thisUser = userBase / user
+
+      /** https://www.hipchat.com/docs/apiv2/method/view_user */
+      def apply[T]
+        (handler: Client.Handler[T])
+        (implicit ec: ExecutionContext)=
+        request(thisUser)(handler)
+
+      def message(msg: String) =
+        complete(thisUser.POST << compact(render(
+          "message" -> msg)))
+
+      /** https://www.hipchat.com/docs/apiv2/method/delete_user */
+      def delete =
+        complete(thisUser.DELETE)
+
+      /** https://www.hipchat.com/docs/apiv2/method/update_user */
+      //def update()
+
+      /** https://www.hipchat.com/docs/apiv2/method/create_user */
+      // def create()
+    }
+
+    def get(user: String) = User(user)
+
     case class Editor(
       userId: Option[String]    = None,
       _email: Option[String]    = None,
@@ -87,38 +194,37 @@ trait Methods { self: Requests =>
       def apply[T]
         (handler: Client.Handler[T])
         (implicit ec: ExecutionContext) =
-        request(endpoint << Map.empty[String, String] ++
-              userId.map("user_id" -> _) ++
-              _email.map("email" -> _) ++
-              _name.map("name" -> _) ++
-              _mention.map("mention_name" -> _) ++
-              _title.map("title" -> _) ++
-              _admin.map("is_group_admin" -> Bool(_)) ++
-              _password.map("password" -> _) ++
-              _timezone.map("timezone" -> _))(handler)
+        request(endpoint << compact(render(
+              ("user_id"     -> userId) ~
+              ("email"       -> _email) ~
+              ("name"        -> _name) ~
+              ("mention_name" -> _mention) ~
+              ("title"       -> _title) ~
+              ("is_group_admin" -> _admin) ~
+              ("password"    -> _password) ~
+              ("timezone"    -> _timezone))))(handler)
 
       private def endpoint =
-        usersBase.POST / userId.map(
-          Function.const("updated")).getOrElse("create")
+        userId.map(userBase.PUT / _).getOrElse(userBase.POST)
     }
 
+    /** https://www.hipchat.com/docs/apiv2/method/create_user */
     def create(
       email: String,
       name: String) = Editor().email(email).name(name)
 
     def update(userId: String) = Editor(Some(userId))
 
-    def delete(userId: String) =
-      complete(usersBase.POST / "delete" << Map("user_id" -> userId))
-
-    def undelete(userId: String) =
-      complete(usersBase.POST / "undelete" << Map("user_id" -> userId))
-
-    def users(withDeleted: Boolean = false) =
-      complete(usersBase / "list" <<? Map(
-        "include_deleted" -> Bool(withDeleted)))
-
-    def show(userId: String) =
-      complete(usersBase / "show" <<? Map("user_id" -> userId))
+    /** https://www.hipchat.com/docs/apiv2/method/get_all_users */
+    def apply[T]
+      (start: Int = 0, max: Int = 100,
+       guests: Boolean = false, deleted: Boolean = false)
+      (handler: Client.Handler[T])
+      (implicit ec: ExecutionContext) =
+      request(userBase <<? Map(
+        "start-index" -> start.toString,
+        "max-results" -> max.toString,
+        "include-guests" -> guests.toString,
+        "include-deleted" -> deleted.toString))(handler)
   }
 }
